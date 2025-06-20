0b0e64b5d68d64d61536accf435d39e381ee2a77
Maria Peregudova <marie.peregudova@gmail.com>
2025-06-20 22:20:34 +0200

    Update generatorPacjentow.cpp
    dodałam tutaj funkcję search

--- generatorPacjentow.cpp
+++ generatorPacjentow.cpp
@@ -3,6 +3,8 @@
 #include <fstream> // działania na plikach
 #include <string> // stringi i ich metody np. back()
 #include <ctime> // time()
+#include <vector> //potrzebne dla funkcji split_text
+#include <sstream> //potrzebne dla funkcji split_text
 #include "generator_pacjentow.hpp"
 
 using namespace std;
@@ -35,8 +37,6 @@ int Pomiary::get_temp() const {
 int Pomiary::get_stan() const {
     return stan;
 }
-
-
 
 // settery
 void Pomiary::set_cS(int wartosc) {
@@ -248,6 +248,155 @@ string wygenerujPesel()
     }
     return pesel;
 }
+
+//funkcje potrzebne do działania search
+const int ile = 20;
+Pacjent tab[ile];
+
+std::vector <std::string> split_text(const std::string &text, char delimiter){ //funkcja zwracająca wektor stringów, podzielonych delimiterem                                           
+    std::vector<std::string> tokens;
+    std::stringstream ss(text);
+    std::string token;
+
+    while(std::getline(ss, token, delimiter)){
+	tokens.push_back(token);
+    }
+    return tokens;
+}
+
+int wczytaj(Pacjent *tab){ //funkcja wczytująca z pliku dane pacjentów 'obecnych' w szpitalu
+    int count = 0;
+    std::string filename, line;
+    std::vector<std::string> tokens;
+    std::ifstream file;
+    filename = "dane.txt";
+    file.open(filename.c_str());
+    while(std::getline(file, line)){
+        tokens = split_text(line, ' ');
+        tab[count].set_pacjent( std::stoi(tokens[0]), tokens[1], tokens[2], tokens[3], std::stoi(tokens[4]), tokens[5], std::stoi(tokens[6]), std::stoi(tokens[7]), std::stoi(tokens[7]), std::stoi(tokens[8]), std::stoi(tokens[9]), std::stoi(tokens[10]), std::stoi(tokens[11]), std::stoi(tokens[12]), std::stoi(tokens[13]), std::stoi(tokens[14]), tokens[15]);
+	    count++;
+    }
+    file.close();
+    return count;
+}
+
+  const char* msg_0 = "Aby uruchomiś funkcję wyszukiwania wybierz 1\nAby zakończyć działanie programu wybierz 0\n";  
+  const char* msg_exit = " Chcę wyjść z funkcji poszukującej - -1\n";
+  const char* msg_01 = "Wybierz po jakim parametrze chcesz wyszukać:\n imie - 1 \n nazwisko - 2 \n stan - 3 \n pokój - 4 \n opiekujący się pacjentem Lekarz(nazwisko) - 5 \n";
+  const char* msg_001 = "Wprowadź imię albo jego fragment:\n";
+  const char* msg_0001 = "Wprowadź nazwisko albo jego fragment:\n";
+  const char* msg_002 = "Wprowadź stan pacjentów, którzych chcesz znaleźć. Uwaga! Stan -  1-5\n";
+  const char* msg_003 = "Wprowadź numer pokoju, w którym chcesz znaleźć pacjentów. Uwaga! Pokoje -  1-2\n";
+
+void search(Pacjent *tab){
+  while(end){    
+    std::cout<<msg_01<<msg_exit;                                                                                                                                                       
+    std::cin>>choice; //Po czym chcemy szukać
+    
+    std::string name;
+    int number = 0;
+    int count = 0;
+    int found = 0;
+    if(choice == -1) {end = 0;}
+    
+    else if(choice == 1) { //imie                                                                                                                                                       
+      std::cout<<msg_001<<msg_exit;
+      std::cin>>name;
+      if(name == "-1") {end = 0;}
+      else{
+	    count = wczytaj(tab);
+	    for(int i=0;i<count;i++){
+	      std::string data = tab[i].get_imie();
+	      found = data.find(name);
+	      if(found != std::string::npos){
+	            std::cout<<tab[i].get_id()<<" "<<tab[i].get_imie()<<" "<<tab[i].get_nazwisko()<<" "<<tab[i].get_stan()<<" "<<tab[i].get_pokoj()<<" "<<tab[i].get_lekarz()<<"\n";
+	        }
+	      found = 0;
+	       }
+         }
+        count = 0;
+    }
+    
+    else if(choice == 2) { //nazwisko                                                                                                                                                   
+      std::cout<<msg_0001<<msg_exit;
+      std::cin>>name;
+      if(name == "-1") {end = 0;}
+      else{
+    	count = wczytaj(tab);
+        for(int i=0;i<count;i++){
+          std::string data = tab[i].get_nazwisko();
+          found = data.find(name);
+          if(found != std::string::npos){
+            std::cout<<tab[i].get_id()<<" "<<tab[i].get_imie()<<" "<<tab[i].get_nazwisko()<<" "<<tab[i].get_stan()<<" "<<tab[i].get_pokoj()<<" "<<tab[i].get_lekarz()<<"\n";
+          }
+          found = 0;
+        }
+      }
+    count = 0;
+    }
+    
+    else if(choice == 3) { //stan                                                                                                                                                       
+      std::cout<<msg_002<<msg_exit;
+      std::cin>>number; //To wartość stanu, którą chcemy wyszukać
+      if(number == -1){ end = 0;}
+      else{
+	    count = wczytaj(tab);
+	    for(int i=0;i<count;i++){
+	      int data = tab[i].get_stan();
+	      if(data == number){
+	        std::cout<<tab[i].get_id()<<" "<<tab[i].get_imie()<<" "<<tab[i].get_nazwisko()<<" "<<tab[i].get_stan()<<" "<<tab[i].get_pokoj()<<" "<<tab[i].get_lekarz()<<"\n";
+	      }
+	      data = 0;
+	    }
+      }
+      count = 0;
+      number = 0;
+    }
+    else if(choice == 4) { //pokój                                                                                                                                                      
+      std::cout<<msg_003;
+      std::cin>>number; //To numer pokoju, który chcemy wyszukać
+      if(number == -1){ end = 0;}
+      else{
+	    count = wczytaj(tab);
+	    for(int i=0;i<count;i++){
+          int data = tab[i].get_id();
+          if(data%2 == number%2){
+            std::cout<<tab[i].get_id()<<" "<<tab[i].get_imie()<<" "<<tab[i].get_nazwisko()<<" "<<tab[i].get_stan()<<" "<<tab[i].get_pokoj()<<" "<<tab[i].get_lekarz()<<"\n";
+          }
+          data = 0;
+	}
+      }
+      count = 0;
+      number = 0;
+
+    }
+    else if(choice == 5) {
+      std::cout<<msg_0001<<msg_exit;
+      std::cin>>name; //pełne, czy fragment z prawej lub lewej                                                                                                                       
+      if(name == "-1") {end = 0;}
+      else{
+        count = wczytaj(tab);
+        for(int i=0;i<count;i++){
+          std::string data = tab[i].get_lekarz();
+          found = data.find(name);
+          if(found != std::string::npos){
+            std::cout<<tab[i].get_id()<<" "<<tab[i].get_imie()<<" "<<tab[i].get_nazwisko()<<" "<<tab[i].get_stan()<<" "<<tab[i].get_pokoj()<<" "<<tab[i].get_lekarz()<<"\n";
+          }
+          found = 0;
+        }
+	
+      }
+      count = 0;
+
+    }
+      
+    std::cout<<msg_0;
+    std::cin>>end;
+  }
+  
+}
+
+
 
 // int main()
 // {
